2aa2c31b95e11d89824d43cc386f7944adbe6096
laciecoder <pvrash1256@gmail.com>
2025-03-31 22:22:06 +0530

    practice restart

---
+++ A_Dislike_of_Threes.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb push_back
+#define sz(a) (int)a.size()
+#define int ll
+#define print(x) cout << x << "\n"
+#define vi vector<int>
+#define INF LLONG_MAX
+#define NINF LLONG_MIN
+
+void solve(){
+  int n;
+  cin >> n;
+  for(int i = 1; ; i++){
+    if(i % 3 == 0 || i % 10 == 3)
+      continue;
+    if(--n == 0){
+      print(i);
+      return;
+    }
+  }
+
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
